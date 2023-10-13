43fa852184cb4747ec89f397ae0ef210ebeb38c2
pleahmacaka <pleahmacaka@gmail.com>
2023-10-13 15:15:34 +0900

    chore(scss): add path/charset config and rm shit

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ import { vitePreprocess } from "@sveltejs/kit/vite"
 const config = {
     preprocess: vitePreprocess({
         scss: {
-            prependData: "@import './src/lib/scss/base.scss';"
+            prependData: `@import "./src/lib/scss/base.scss";`,
+            charset: 'UTF-8',
+            includePaths: ['src/lib/scss']
         }
     }),
     kit: {
