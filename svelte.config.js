c16723a0b49269f7626f439af071a0d67f88baca
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-06 01:53:10 +0900

    chore: remove shit wtf

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,14 @@
-import adapter from "@sveltejs/adapter-static"
+// noinspection JSUnresolvedReference
+
+import adapter from "@sveltejs/adapter-auto"
 import { vitePreprocess } from "@sveltejs/kit/vite"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-    preprocess: vitePreprocess({
-        scss: {
-            prependData: `@import "./src/lib/scss/base.scss";`,
-            charset: 'UTF-8',
-            includePaths: ['src/lib/scss']
-        }
-    }),
+    preprocess: vitePreprocess(),
     kit: {
         adapter: adapter()
-    },
+    }
 }
 
 // noinspection JSUnusedGlobalSymbols
