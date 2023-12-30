e181030d462e11353fa16ec6f6a360a5a4575c45
PleahMaCaka <pleahmacaka@gmail.com>
2023-12-30 18:38:26 +0900

    Reformat all

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 - **Tauri** - w/ Svelte
 - **Tailwind CSS** and **SASS/SCSS** by default
 - **ESLint** and **Prettier** is ready
-  - with [prettier-plugin-svelte](https://github.com/sveltejs/prettier-plugin-svelte) and [eslint-plugin-svelte](https://github.com/sveltejs/eslint-plugin-svelte)
+  - with [prettier-plugin-svelte](https://github.com/sveltejs/prettier-plugin-svelte)
+    and [eslint-plugin-svelte](https://github.com/sveltejs/eslint-plugin-svelte)
 - **GitHub Actions** for build / eslint / prettier check and tag-based auto release
 
 ## Development
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -21,19 +21,19 @@
   }
 </script>
 
-<div data-tauri-drag-region
-     class="h-8 flex justify-between items-center bg-primary select-none rounded-t-xl px-2"
+<div class="h-8 flex justify-between items-center bg-primary select-none rounded-t-xl px-2"
+     data-tauri-drag-region
 >
-  <img class="h-6 w-6" alt="Logo" src="/favicon.png" />
+  <img alt="Logo" class="h-6 w-6" src="/favicon.png" />
   <p class="absolute left-1/2 transform -translate-x-1/2 font-bold text-white">Tauri Svelte Template!</p>
   <div class="flex items-center justify-between w-20">
-    <button on:click={onMinimize} class="toolbar-btn">
+    <button class="toolbar-btn" on:click={onMinimize}>
       <img alt="Minimize" src="/toolbar/minimize.svg" />
     </button>
-    <button on:click={onMaximize} class="toolbar-btn">
+    <button class="toolbar-btn" on:click={onMaximize}>
       <img alt="Maximize" src="/toolbar/maximize.svg" />
     </button>
-    <button on:click={onClose} class="toolbar-btn">
+    <button class="toolbar-btn" on:click={onClose}>
       <img alt="Close" src="/toolbar/close.svg" />
     </button>
   </div>
